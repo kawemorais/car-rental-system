8624b92e7d2822b32e818902965c68c3684fcdf6
kawemorais <mcgamerz2013@gmail.com>
2023-04-14 22:35:12 -0300

    Initial project configuration

---
+++ src/test/java/br/com/system/carrental/CarrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.system.carrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
