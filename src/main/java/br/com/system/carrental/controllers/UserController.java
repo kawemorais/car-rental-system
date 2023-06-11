9a388bb8ac6386a4176cc13e391f62fe0b86e65f
kawemorais <mcgamerz2013@gmail.com>
2023-06-11 15:43:27 -0300

    refactor: Changed the dependency injector of UserServiceImpl to class constructor instead of using @Autowired

--- src/main/java/br/com/system/carrental/controllers/UserController.java
+++ src/main/java/br/com/system/carrental/controllers/UserController.java
@@ -1,11 +1,10 @@
 package br.com.system.carrental.controllers;
 
-import br.com.system.carrental.dtos.userDTO.UserRequestDTO;
-import br.com.system.carrental.dtos.userDTO.UserResponseDTO;
-import br.com.system.carrental.exception.UserNotFoundExeption;
-import br.com.system.carrental.services.UserServiceImpl;
+import br.com.system.carrental.dtos.user.UserRequestDTO;
+import br.com.system.carrental.dtos.user.UserResponseDTO;
+import br.com.system.carrental.exception.EntityNotFoundExeption;
+import br.com.system.carrental.services.user.UserServiceImpl;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,29 +16,32 @@ import java.util.Optional;
 @RequestMapping("/api/v1/user")
 public class UserController {
 
-    @Autowired
-    UserServiceImpl userServiceImpl;
+    private final UserServiceImpl userService;
+
+    public UserController(UserServiceImpl userServiceImpl) {
+        this.userService = userServiceImpl;
+    }
 
     @GetMapping
     public ResponseEntity<List<UserResponseDTO>> findAllUsers(){
-        List<UserResponseDTO> userList = userServiceImpl.listAllUsers();
+        List<UserResponseDTO> userList = userService.listAllUsers();
         return new ResponseEntity<>(userList, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> findUserById(@PathVariable Long id){
-        Optional<UserResponseDTO> user = userServiceImpl.findUserById(id);
+        Optional<UserResponseDTO> user = userService.findUserById(id);
 
         if (user.isPresent()){
             return new ResponseEntity<>(user.get(), HttpStatus.OK);
         }
 
-        throw new UserNotFoundExeption("Usuário ID: " + id + " não encontrado");
+        throw new EntityNotFoundExeption("Usuário ID: " + id + " não encontrado");
     }
 
     @PostMapping
     public ResponseEntity<UserResponseDTO> createUser(@RequestBody @Valid UserRequestDTO userRequestDTO) throws Exception {
-        UserResponseDTO userResponseDTO = userServiceImpl.createUser(userRequestDTO);
+        UserResponseDTO userResponseDTO = userService.createUser(userRequestDTO);
         return new ResponseEntity<>(userResponseDTO, HttpStatus.CREATED);
     }
 
@@ -47,22 +49,22 @@ public class UserController {
     public ResponseEntity<UserResponseDTO> updateUserById(@PathVariable Long id,
                                                           @RequestBody @Valid UserRequestDTO userRequestDTO){
 
-        Optional<UserResponseDTO> userResponseDTO = userServiceImpl.updateUserById(id, userRequestDTO);
+        Optional<UserResponseDTO> userResponseDTO = userService.updateUserById(id, userRequestDTO);
 
         if(userResponseDTO.isPresent()) {
             return new ResponseEntity<>(userResponseDTO.get(), HttpStatus.OK);
         }
 
-        throw new UserNotFoundExeption("Usuário ID: " + id + " não encontrado");
+        throw new EntityNotFoundExeption("Usuário ID: " + id + " não encontrado");
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUserById(@PathVariable Long id){
 
-        Optional<Boolean> isDeleted = userServiceImpl.deleteUserById(id);
+        Optional<Boolean> isDeleted = userService.deleteUserById(id);
 
         if(isDeleted.isEmpty()){
-            throw new UserNotFoundExeption("Usuário ID: " + id + " não encontrado");
+            throw new EntityNotFoundExeption("Usuário ID: " + id + " não encontrado");
         }
 
         return new ResponseEntity<>(HttpStatus.OK);
