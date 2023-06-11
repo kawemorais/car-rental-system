6b3a6fd1a48826d1abf1c10bb24d0895909760c0
kawemorais <mcgamerz2013@gmail.com>
2023-06-11 01:48:05 -0300

    feat: Added SpringDoc OpenAPI swagger for api documentation

--- src/main/java/br/com/system/carrental/CarrentalApplication.java
+++ src/main/java/br/com/system/carrental/CarrentalApplication.java
@@ -1,10 +1,20 @@
 package br.com.system.carrental;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication()
+@OpenAPIDefinition(
+		info = @Info(
+				title = "System CarRental - API Documentation",
+				description = "This is an api to a car rental system",
+				contact = @Contact(name = "Kawe M. Carvalho", url = "https://github.com/kawemorais/car-rental-system"),
+				version = "1.0"
+		)
+)
 public class CarrentalApplication {
 
 	public static void main(String[] args) {
